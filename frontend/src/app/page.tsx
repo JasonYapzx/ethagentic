20f7394dcc3f05abc7b67333694cf807e725ea6f
Jason Yap <jasonyapzx@gmail.com>
2025-02-03 23:54:51 +0800

    feat: init frontend with frontend demo page

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,22 @@
+import { CTA } from "@/components/landing/cta";
+import { Examples } from "@/components/landing/examples";
+import { Features } from "@/components/landing/features";
+import { Footer } from "@/components/landing/footer";
+import { Header } from "@/components/landing/header";
+import { Hero } from "@/components/landing/hero";
+import { UseCases } from "@/components/landing/use-cases";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <main >
+        <Header />
+        <Hero />
+        <Examples />
+        <UseCases />
+        <Features />
+        <CTA />
+        <Footer />
+      </main>
+  );
+}
